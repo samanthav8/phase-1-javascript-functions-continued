98e88a4da196c3879219d3cac2811b0497a3a365
Samantha Villafana <samanthavillafana8@gmail.com>
2023-11-06 16:30:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity){
+    if (activity){
+        return `This Monday, I will ${activity}.`;
+    } else {
+        return "This Monday, I will go to the office.";
+    }
+}
+
+function wrapAdjective(flair = '*'){
+    return function(adjective= 'special'){
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+
+
